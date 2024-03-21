6011e8a0d793509b6f1ac70886bae64a99ab4c7e
Denys <babychdenya@gmail.com>
2024-03-21 19:14:14 +0000

    feat: start writing userService tests

--- src/filter/filters.service.spec.ts
+++ src/filter/filters.service.spec.ts
@@ -3,9 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing'
 
 import { Model } from 'mongoose'
 
-import { Glass } from 'schemas'
-import { Category } from 'schemas/category.schema'
-import { Ingredient } from 'schemas/ingredient.schema'
+import { Category, Glass, Ingredient } from 'schemas'
 
 import { FilterService } from './filters.service'
 
@@ -54,87 +52,83 @@ describe('filterService', () => {
     )
   })
 
-  describe('findAll - categories', () => {
-    it('should return an array of categories', async () => {
-      const result = [{ id: '65e239dc2410175666785c6d', name: 'Cocoa' }]
+  it('should be defined', () => {
+    expect(service).toBeDefined()
+  })
 
-      const findSpy = jest
-        .spyOn(categoryModel, 'find')
-        .mockResolvedValue(result)
+  it('should return an array of categories', async () => {
+    const result = [{ id: '65e239dc2410175666785c6d', name: 'Cocoa' }]
 
-      const categories = await service.getAllCategories()
+    const findSpy = jest.spyOn(categoryModel, 'find').mockResolvedValue(result)
 
-      expect(findSpy).toHaveBeenCalledTimes(1)
+    const categories = await service.getAllCategories()
 
-      expect(categories).toStrictEqual(result)
+    expect(findSpy).toHaveBeenCalledTimes(1)
 
-      expect(categories).toHaveLength(result.length)
+    expect(categories).toStrictEqual(result)
 
-      categories.forEach(category => {
-        expect(category).toHaveProperty('id')
-        expect(category).toHaveProperty('name')
-      })
+    expect(categories).toHaveLength(result.length)
+
+    categories.forEach(category => {
+      expect(category).toHaveProperty('id')
+      expect(category).toHaveProperty('name')
     })
   })
 
-  describe('findAll - glasses', () => {
-    it('should return an array of glasses', async () => {
-      const result = [{ id: '65e239dc2410175666785c6d', name: 'Jar' }]
+  it('should return an array of glasses', async () => {
+    const result = [{ id: '65e239dc2410175666785c6d', name: 'Jar' }]
 
-      const findSpy = jest.spyOn(glassModel, 'find').mockResolvedValue(result)
+    const findSpy = jest.spyOn(glassModel, 'find').mockResolvedValue(result)
 
-      const glasses = await service.getAllGlasses()
+    const glasses = await service.getAllGlasses()
 
-      expect(findSpy).toHaveBeenCalledTimes(1)
+    expect(findSpy).toHaveBeenCalledTimes(1)
 
-      expect(glasses).toStrictEqual(result)
+    expect(glasses).toStrictEqual(result)
 
-      expect(glasses).toHaveLength(result.length)
+    expect(glasses).toHaveLength(result.length)
 
-      glasses.forEach(glass => {
-        expect(glass).toHaveProperty('id')
-        expect(glass).toHaveProperty('name')
-      })
+    glasses.forEach(glass => {
+      expect(glass).toHaveProperty('id')
+      expect(glass).toHaveProperty('name')
     })
   })
 
-  describe('findAll - ingredients', () => {
-    it('should return an array of ingredients', async () => {
-      const result = [
-        {
-          title: 'Tea',
-          ingredientThumb:
-            'https://ftp.goit.study/img/drinkify/ingredients/Tea.png',
-          'thumb-medium':
-            'https://ftp.goit.study/img/drinkify/ingredients/Tea-Medium.png',
-          'thumb-small':
-            'https://ftp.goit.study/img/drinkify/ingredients/Tea-Small.png',
-          abv: '0',
-          alcohol: 'No',
-          description:
-            "Tea is a popular aromatic beverage made by steeping the dried leaves of the Camellia sinensis plant in hot water. It's one of the most widely consumed beverages in the world, known for its diverse flavours and potential health benefits.",
-          type: 'Tea',
-          flavour: 'tea',
-          country: 'United States',
-          id: '64aebb7f82d96cc69e0eb4b4'
-        }
-      ]
+  it('should return an array of ingredients', async () => {
+    const result = [
+      {
+        title: 'Tea',
+        ingredientThumb:
+          'https://ftp.goit.study/img/drinkify/ingredients/Tea.png',
+        'thumb-medium':
+          'https://ftp.goit.study/img/drinkify/ingredients/Tea-Medium.png',
+        'thumb-small':
+          'https://ftp.goit.study/img/drinkify/ingredients/Tea-Small.png',
+        abv: '0',
+        alcohol: 'No',
+        description:
+          "Tea is a popular aromatic beverage made by steeping the dried leaves of the Camellia sinensis plant in hot water. It's one of the most widely consumed beverages in the world, known for its diverse flavours and potential health benefits.",
+        type: 'Tea',
+        flavour: 'tea',
+        country: 'United States',
+        id: '64aebb7f82d96cc69e0eb4b4'
+      }
+    ]
 
-      const findSpy = jest
-        .spyOn(ingredientModel, 'find')
-        .mockResolvedValue(result)
+    const findSpy = jest
+      .spyOn(ingredientModel, 'find')
+      .mockResolvedValue(result)
 
-      const ingredients = await service.getAllIngredients()
+    const ingredients = await service.getAllIngredients()
 
-      expect(findSpy).toHaveBeenCalledTimes(1)
+    expect(findSpy).toHaveBeenCalledTimes(1)
 
-      expect(ingredients).toStrictEqual(result)
+    expect(ingredients).toStrictEqual(result)
 
-      expect(ingredients).toHaveLength(result.length)
+    expect(ingredients).toHaveLength(result.length)
 
-      ingredients.forEach(ingredient => {
-        expect(ingredient).toMatchObject(result[0])
-      })
+    ingredients.forEach(ingredient => {
+      expect(ingredient).toMatchObject(result[0])
     })
   })
 })
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,25 +1,54 @@
-// import { getModelToken } from '@nestjs/mongoose'
-// import { Test, TestingModule } from '@nestjs/testing'
+import { NotFoundException } from '@nestjs/common'
+import { getModelToken } from '@nestjs/mongoose'
+import { Test, TestingModule } from '@nestjs/testing'
 
-// import { User } from 'schemas/user.schema'
+import { MailerService } from '@nestjs-modules/mailer'
+import { Model } from 'mongoose'
 
-// import { UserService } from './user.service'
+import { User } from 'schemas/user.schema'
 
-// describe('UsersService', () => {
-//   let service: UserService
+import { UserService } from './user.service'
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [
-//         UserService,
-//         { provide: getModelToken(User.name), useValue: {} }
-//       ]
-//     }).compile()
+describe('userService', () => {
+  let service: UserService
+  let model: Model<User>
+  let mailerService: MailerService
 
-//     service = module.get<UserService>(UserService)
-//   })
+  const mockAuthService = {
+    create: jest.fn(),
+    findByIdAndUpdate: jest.fn()
+  }
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined()
-//   })
-// })
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        { provide: getModelToken(User.name), useValue: {} },
+        { provide: MailerService, useValue: {} },
+        {
+          provide: getModelToken(User.name),
+          useValue: mockAuthService
+        }
+      ]
+    }).compile()
+
+    service = module.get<UserService>(UserService)
+    model = module.get<Model<User>>(getModelToken(User.name))
+    mailerService = module.get<MailerService>(MailerService)
+  })
+
+  it('should be defined', () => {
+    expect(service).toBeDefined()
+  })
+
+  describe('update', () => {
+    it('should throw NotFoundException if user not found', async () => {
+      jest.spyOn(model, 'findByIdAndUpdate').mockResolvedValueOnce(null)
+
+      const id = 'id'
+      const dto = { password: 'password' }
+
+      await expect(service.update(id, dto)).rejects.toThrow(NotFoundException)
+    })
+  })
+})
